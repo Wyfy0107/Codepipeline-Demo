35937c455df28f8fdac4deeea1e63ed1443bdd3b
wyfy <nguyenduy010798@gmail.com>
2021-01-17 02:03:16 +0200

    test

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const app = require('express')();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('hello'));
+app.get('/test', (req, res) => res.send('yo'));
 app.listen(PORT);
